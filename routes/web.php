21d5f39eeb4ee3e8edfd1b09b1e8133a25c7b44d
bishal <csit22081043_bishal@achsnepal.edu.np>
2025-12-20 14:43:46 +0545

    auth and login logout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\AuthAdmin;
+use App\Models\User;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
