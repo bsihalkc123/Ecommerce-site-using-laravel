8bd3ebcbfd93a5decb684d08c72db3f2a1ff814d
bishal <csit22081043_bishal@achsnepal.edu.np>
2025-12-25 14:07:22 +0545

    update dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Brand;
 
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
- public function index(){
+ public function index()
+ {
         return view('admin.index');  // Logic to list admins
- }   //
+ } 
+ public function brands()
+ {
+       $brands = Brand::orderBy('id','Desc')->paginate(10); 
+       return view('admin.brands', compact('brands')); // Logic to list brands
+ }
 }
